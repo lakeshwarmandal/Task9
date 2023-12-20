b48ae9efd5c645c176a881bf45943bc4757cdeea
Lakeshwar <110439492+lakeshwarmandal@users.noreply.github.com>
2023-12-20 16:01:07 -0600

    Task9

---
+++ src/App.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Task 1", completed: false },
+    { id: 2, title: "Task 2", completed: true },
+  ]);
+  const addTask = (title) => {
+    setTasks([...tasks, { id: tasks.length + 1, title, completed: false }]);
+  };
+  const editTask = (id, newTitle) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, title: newTitle } : task
+      )
+    );
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.title}
+            <button
+              className="button"
+              onClick={() =>
+                editTask(task.id, prompt("Enter new title:", task.title))
+              }
+            >
+              Edit
+            </button>
+            <button className="delete" onClick={() => deleteTask(task.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const title = e.target.elements.title.value;
+          addTask(title);
+          e.target.elements.title.value = "";
+        }}
+      >
+        <input
+          className="text"
+          type="text"
+          name="title"
+          placeholder="Enter Task Title"
+          required
+        />
+        <button type="submit">Add Task / Submit</button>
+        <div className="link">
+          <button className="git">
+            <a href="https://github.com/">Git</a>
+          </button>
+          <button className="net">
+            <a href="https://app.netlify.com/teams/lakeshwarmandal/overview">
+              Netlify
+            </a>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default App;
